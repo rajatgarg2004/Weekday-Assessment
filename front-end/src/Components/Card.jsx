454ad9b117466607a32ba8e67beab122948a273c
rajatgarg2004 <rajatgarg.bt21ele@pec.edu.in>
2024-05-04 14:53:55 +0530

    Add All Cards

--- front-end/src/Components/Card.jsx
+++ front-end/src/Components/Card.jsx
@@ -1,7 +1,8 @@
 import {useState} from 'react';
-export default function Card(){
-    const data = {"jdLink":"https://weekday.works","jobDetailsFromCompany":"This is a sample job and you must have displayed it to understand that its not just some random lorem ipsum text but something which was manually written. Oh well, if random text is what you were looking for then here it is: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and now in this assignment.","maxJdSalary":61,"minJdSalary":null,"salaryCurrencyCode":"USD","location":"delhi ncr","minExp":3,"maxExp":6,"jobRole":"frontend","companyName":"Dropbox","logoUrl":"https://logo.clearbit.com/dropbox.com"};
+export default function Card(props){
+    // const data = {"jdLink":"https://weekday.works","jobDetailsFromCompany":"This is a sample job and you must have displayed it to understand that its not just some random lorem ipsum text but something which was manually written. Oh well, if random text is what you were looking for then here it is: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and now in this assignment.","maxJdSalary":61,"minJdSalary":null,"salaryCurrencyCode":"USD","location":"delhi ncr","minExp":3,"maxExp":6,"jobRole":"frontend","companyName":"Dropbox","logoUrl":"https://logo.clearbit.com/dropbox.com"};
     const [expanded, setExpanded] = useState(false);
+    const data = props.data;
 
     const toggleDescription = () => {
         setExpanded(!expanded);
@@ -12,25 +13,25 @@ export default function Card(){
                 <div className="flex flex-col w-[30%]">
                     <img src={data.logoUrl} alt="logo"/>
                 </div>
-                <div class="flex flex-col justify-between w-[60%]">
-                    <span className="font-bold">{data.companyName}</span>
-                    <span className="">{data.jobRole}</span>
-                    <span>{data.location}</span>
+                <div class="flex flex-col justify-between w-[60%] pl-4">
+                    <span className="text-left font-bold">{data.companyName}</span>
+                    <span className="text-left">{data.jobRole}</span>
+                    <span className='text-left'>{data.location}</span>
                 </div>  
             </div>
             <span className="mt-2">Estimated Salary: 
                 {
                     data.minJdSalary && data.maxJdSalary?
                     <>
-                         {data.salaryCurrencyCode}{data.minJdSalary} - {data.maxJdSalary} LPA
+                         {data.salaryCurrencyCode} {data.minJdSalary} - {data.maxJdSalary} LPA
                     </>:
                     data.minJdSalary?
                     <>
-                        Min-{data.salaryCurrencyCode} {data.minJdSalary} LPA
+                        Min- {data.salaryCurrencyCode} {data.minJdSalary} LPA
                     </>:
                     data.maxJdSalary?
                     <>
-                        Max-{data.salaryCurrencyCode} {data.maxJdSalary} LPA
+                        Max- {data.salaryCurrencyCode} {data.maxJdSalary} LPA
                     </>:
                     <>
                         No Salary Details
--- front-end/src/Components/Cards.jsx
+++ front-end/src/Components/Cards.jsx
@@ -1,11 +1,12 @@
-import Card from "./Card";
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
 
 export default function Cards() {
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
     const body = JSON.stringify({
-        "limit": 10,
+        "limit": 12,
         "offset": 0
     });
 
@@ -15,11 +16,32 @@ export default function Cards() {
         body
     };
 
-    fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.error(error));
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        async function loadData() {
+            try {
+                const response = fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
+                .then((response) => response.json())
+                .then((result) => {setData(result)})
+                .catch((error) => console.error(error));
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        loadData();
+    }, []); 
     return (
-        <Card />
+        <div className='flex flex-wrap'>
+            {data ? (
+                data.jdList.map((job, index) => (
+                    <div className='w-[30%] p-10' key={index}>
+                        <Card data={job} />
+                    </div>
+                ))
+            ) : (
+                <h1>No Data</h1>
+            )}
+        </div>
     );
 }
